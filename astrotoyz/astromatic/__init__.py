6ce941ca8d6f2d9b77002125130b344c624c3c2b
Fred Moolekamp <fred3public@gmail.com>
2015-05-12 18:25:21 -0400

    Use tempfile.NamedTemporaryFile for convert_table_to_ldac

--- astrotoyz/astromatic/__init__.py
+++ astrotoyz/astromatic/__init__.py
@@ -1,5 +1,1 @@
-from astrotoyz.astromatic import sex
-from astrotoyz.astromatic import scamp
-from astrotoyz.astromatic import swarp
-from astrotoyz.astromatic import psfex
 from astrotoyz.astromatic import api
