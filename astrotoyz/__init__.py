baf3a993d539337fa02d78ca518329385951149a
Fred Moolekamp <fred3public@gmail.com>
2015-06-10 15:55:43 -0400

    Removed astromatic module from astrotoyz

--- astrotoyz/__init__.py
+++ astrotoyz/__init__.py
@@ -1,7 +1,6 @@
 from astrotoyz import tasks
 from astrotoyz import viewer
 from astrotoyz import detect_sources
-from astrotoyz import astromatic
 #from astrotoyz import catalog
 from astrotoyz import io
 from astrotoyz import data_types
