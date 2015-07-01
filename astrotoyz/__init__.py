97203c7ee042b8708c5590b7f2f64cda58c7c089
fred3m <fred3public@gmail.com>
2015-06-30 20:17:24 -0400

    Converted astrotoyz to astropy affiliated package

--- astrotoyz/__init__.py
+++ astrotoyz/__init__.py
@@ -1,8 +1,22 @@
+# Licensed under a 3-clause BSD style license - see LICENSE.rst
+
+"""
+This is an Astropy affiliated package.
+"""
+
+# Affiliated packages may add whatever they like to this file, but
+# should keep this content at the top.
+# ----------------------------------------------------------------------------
+from ._astropy_init import *
+# ----------------------------------------------------------------------------
+
+# For egg_info test builds to pass, put package imports here.
+if not _ASTROPY_SETUP_:
+    from example_mod import *
+
 from astrotoyz import tasks
 from astrotoyz import viewer
 from astrotoyz import detect_sources
-from astrotoyz import astromatic
-#from astrotoyz import catalog
 from astrotoyz import io
 from astrotoyz import data_types
 from astrotoyz import config
